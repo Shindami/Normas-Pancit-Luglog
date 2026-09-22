95a64704c727912a049fa3105e66c93c7d477992
Shindami <vbakong@gmail.com>
2026-09-22 10:15:37 +0800

    changes

--- components/pages/MenuSection/constant.ts
+++ components/pages/MenuSection/constant.ts
@@ -28,27 +28,32 @@ export const MENU_ITEMS: MenuItemProps[] = [
   },
   {
     image: '/bbq.jpg',
-    name: 'Pancit Palabok',
+    name: 'Adobong Pusit',
     description:
-      'Traditional Filipino rice noodles with savory shrimp sauce, topped with eggs, chicharon, and fresh seafood.',
-    price: '₱250-350',
-    toppings: ['Shrimp', 'Eggs', 'Chicharon', 'Green Onions'],
+      'Adobong Pusit is a tasty squid dish cooked using the popular Filipino adobo method.',
+    price: '₱250-300',
+    toppings: [
+      'Toasted Garlic',
+      'Sautéed Aromatics',
+      'Chilies',
+      'Green Onions',
+    ],
   },
   {
-    image: '/lumpia.jpg',
-    name: 'Fresh Lumpiang Ubod',
+    image: '/asim.jpg',
+    name: 'Sinampalukan Aguot',
     description:
       'Soft, fluffy Filipino steamed rice cakes. Perfect companion to our savory pancit dishes!',
-    price: '₱200',
-    toppings: [],
+    price: '₱100',
+    toppings: ['Usbong ng Sampalok', 'Siling Haba', 'Mustasa'],
   },
   {
-    image: '/backgrounda.jpg',
-    name: 'Pancit Lug-log',
+    image: '/sapsap.jpg',
+    name: 'Pangat sa Kamatis na Sapsap',
     description:
-      'Our signature dish! Thick noodles with rich sauce, topped with generous seafood, eggs, and crispy chicharon. A true Filipino comfort food experience.',
-    price: '₱300-400',
-    toppings: ['Squid', 'Shrimp', 'Eggs', 'Smoked Fish', 'Chicharon', 'Pork'],
+      'fish stew featuring sapsap (slipmouth or ponyfish) gently simmered in a light, watery broth made sour and savory primarily by ripe tomatoes',
+    price: '₱150',
+    toppings: ['Smashed Tomatoes', 'Siling Haba', 'Fresh Leafy Greens'],
     isSpecial: true,
   },
 ];
--- components/pages/OrderOption/index.tsx
+++ components/pages/OrderOption/index.tsx
@@ -2,7 +2,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const OrderOption = ({ icon, title, description, action, delay = 0 }) => {
+interface Props {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+  action: () => void;
+  delay: number;
+}
+
+const OrderOption = ({
+  icon,
+  title,
+  description,
+  action,
+  delay = 0,
+}: Props) => {
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.9 }}
